a087b711be637627810491bcac6d5ffd2354086e
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-07-06 17:46:33 -0300

    Criando a consulta do relatório

--- src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
@@ -19,6 +19,7 @@ import org.springframework.util.StringUtils;
 
 import com.mstadtlober.algamoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.mstadtlober.algamoneyapi.dto.LancamentoEstatisticaDia;
+import com.mstadtlober.algamoneyapi.dto.LancamentoEstatisticaPessoa;
 import com.mstadtlober.algamoneyapi.model.Categoria_;
 import com.mstadtlober.algamoneyapi.model.Lancamento;
 import com.mstadtlober.algamoneyapi.model.Lancamento_;
@@ -30,6 +31,35 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 	
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Override
+	public List<LancamentoEstatisticaPessoa> porPessoa(LocalDate inicio, LocalDate fim) {
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		
+		CriteriaQuery<LancamentoEstatisticaPessoa> criteriaQuery = criteriaBuilder.
+				createQuery(LancamentoEstatisticaPessoa.class);
+		
+		Root<Lancamento> root = criteriaQuery.from(Lancamento.class);
+		
+		criteriaQuery.select(criteriaBuilder.construct(LancamentoEstatisticaPessoa.class,
+				root.get(Lancamento_.tipo),
+				root.get(Lancamento_.pessoa),
+				criteriaBuilder.sum(root.get(Lancamento_.valor))));
+					
+		criteriaQuery.where(
+				criteriaBuilder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+						inicio),
+				criteriaBuilder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+						fim));
+		
+		criteriaQuery.groupBy(root.get(Lancamento_.tipo),
+				root.get(Lancamento_.pessoa));
+		
+		TypedQuery<LancamentoEstatisticaPessoa> typedQuery = manager.
+				createQuery(criteriaQuery);
+		
+		return typedQuery.getResultList();
+	}
 	
 	@Override
 	public List<LancamentoEstatisticaDia> porDia(LocalDate mesReferencia) {
--- src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -8,12 +8,14 @@ import org.springframework.data.domain.Pageable;
 
 import com.mstadtlober.algamoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.mstadtlober.algamoneyapi.dto.LancamentoEstatisticaDia;
+import com.mstadtlober.algamoneyapi.dto.LancamentoEstatisticaPessoa;
 import com.mstadtlober.algamoneyapi.model.Lancamento;
 import com.mstadtlober.algamoneyapi.repository.filter.LancamentoFilter;
 import com.mstadtlober.algamoneyapi.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
 	
+	public List<LancamentoEstatisticaPessoa> porPessoa(LocalDate inicio, LocalDate fim);
 	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 	public List<LancamentoEstatisticaDia> porDia(LocalDate mesReferencia);
 	
