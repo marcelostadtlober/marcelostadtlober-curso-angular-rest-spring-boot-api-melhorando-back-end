94bd5d3f61ae99f51e0e54d6b64c13ff503b225e
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-07-06 23:15:25 -0300

    Buscando lançamentos vencidos com Spring Data JPA

--- src/main/java/com/mstadtlober/algamoneyapi/repository/LancamentoRepository.java
+++ src/main/java/com/mstadtlober/algamoneyapi/repository/LancamentoRepository.java
@@ -1,10 +1,15 @@
 package com.mstadtlober.algamoneyapi.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mstadtlober.algamoneyapi.model.Lancamento;
 import com.mstadtlober.algamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
+	
+	List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 
 }
